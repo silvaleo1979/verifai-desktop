5f916d2984042a67ab821c8d6dc76e2bbc329b85
Gabriel Boian <gabriel.boian@t2cgroup.com.br>
2025-12-03 10:53:38 -0300

    feat: removido a parte do ci do github

--- build/postinstall.mjs
+++ build/postinstall.mjs
@@ -1,12 +1,13 @@
 import { execSync } from "child_process";
 
-const isCI =
-  process.env.CI === "true" ||
-  process.env.GITHUB_ACTIONS === "true" ||
-  process.env.RUNNER_OS === "Windows";
+// const isCI =
+//   process.env.CI === "true" ||
+//   process.env.GITHUB_ACTIONS === "true" ||
+//   process.env.RUNNER_OS === "Windows";
 
-if (isCI) {
-  console.log("Skipping patch-package in CI environment");
-} else {
-  execSync("npx patch-package", { stdio: "inherit" });
-}
+// if (isCI) {
+//   console.log("Skipping patch-package in CI environment");
+// } else {
+//   execSync("npx patch-package", { stdio: "inherit" });
+// }
+execSync("npx patch-package", { stdio: "inherit" });
